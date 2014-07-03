ad8b85ae1a3d5b2c10bb451a76a7b9f7e501c41f
Juan Toledo <toledo+github@lazaro.es>
2014-07-03 13:31:40 +0200

    Generate the thumb_url for each direntry

--- main.py
+++ main.py
@@ -104,9 +104,12 @@ class KBGalleryApp(App):
 
     def got_dirlist(self, req, res):
         Logger.debug("%s: got_dirlist (req %s, results %s" % (APP, req, res))
-        ld = res['listdir']
+
+        turl = self.config.get('general', 'server_url')+'thumb/'
+        ld = [{'direntry': de, 'thumb_url': turl+de}
+              for de in res['listdir']]
+
         data = [(ld[i*2], ld[i*2+1]) for i in range(len(ld)/2)]
-        # import pdb; pdb.set_trace()
         self.root.adapter.data = self.root.adapter.data + data
         self.root._reset_spopulate()
 
